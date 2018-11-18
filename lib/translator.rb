03620677f044e902e110406ed5437ffa725fd1a9
Yuki N <lunarbase@outlook.com>
2018-11-18 20:47:59 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,6 +26,6 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
   
 end
 
-def get_english_meaning
+def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
   # code goes here
 end

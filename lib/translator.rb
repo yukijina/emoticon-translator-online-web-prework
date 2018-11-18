6945d99675d409c72162dc95a0151a08229f9316
Yuki N <lunarbase@outlook.com>
2018-11-18 20:49:28 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,5 +29,6 @@ end
 def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
   # code goes here
    emoticons = load_library(file_path = './lib/emoticons.yml')
-   if emoticons["get_meaning"].include?(japanese_emoticon"]
+   if emoticons["get_meaning"].include?(japanese_emoticon)
+     emoticons["get_meaning"][japanese_emoticon]
 end

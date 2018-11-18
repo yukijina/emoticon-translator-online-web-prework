c2df21f3a21a5830b64a065cfd5473784bac820a
Yuki N <lunarbase@outlook.com>
2018-11-18 20:50:26 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,14 +3,35 @@
 
 def load_library(file_path)
   # code goes here
- 
+  data = YAML.load_file(file_path)
+  
+  emoticons = {"get_meaning" => {}, "get_emoticon" => {}}
+  
+  data.each do |key1, key2|  #key1=angel,angry...  #key2 Eng-emo[0] & Japa-emo[1]
+    emoticons["get_meaning"][key2[1]] = key1
+    emoticons["get_emoticon"][key2[0]] = key2[1]
+  end
+  emoticons
+end
+
+def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
+  # code goes here
+  emoticons = load_library(file_path = './lib/emoticons.yml')
+  
+  if emoticons["get_emoticon"].include?(english_emoticon)
+     emoticons["get_emoticon"][english_emoticon]
+  else 
+    "Sorry, that emoticon was not found"
+  end  
   
 end
 
-def get_japanese_emoticon
+def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
   # code goes here
-end
-
-def get_english_meaning
-  # code goes here
+   emoticons = load_library(file_path = './lib/emoticons.yml')
+   if emoticons["get_meaning"].include?(japanese_emoticon)
+     emoticons["get_meaning"][japanese_emoticon]
+  else 
+    "Sorry, that emoticon was not found"
+  end
 end
